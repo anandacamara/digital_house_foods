8ee664bc156d69cfd7ffb29ac2c7008ff85e9d91
Ananda Câmara <ananda.lr.camara@gmail.com>
2020-11-11 15:51:39 -0300

    Tela cadastro

--- app/src/main/java/com/example/digitalhousefoods/LoginActivity.kt
+++ app/src/main/java/com/example/digitalhousefoods/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.example.digitalhousefoods
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button_register.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
